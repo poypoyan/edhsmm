b2dbafe531911423a4b5126c0ed17b50ec84b3c4
poypoyan <64708703+poypoyan@users.noreply.github.com>
2020-05-07 01:26:48 +0800

    Update hsmm_utils.py
    Removed comments from hmmlearn, and reduced lines

--- edhsmm/hsmm_utils.py
+++ edhsmm/hsmm_utils.py
@@ -1,17 +1,7 @@
 import numpy as np
 
+# masks error when applying log(0)
 # copied from hmmlearn
 def log_mask_zero(a):
-    """Computes the log of input probabilities masking divide by zero in log.
-
-    Notes
-    -----
-    During the M-step of EM-algorithm, very small intermediate start
-    or transition probabilities could be normalized to zero, causing a
-    *RuntimeWarning: divide by zero encountered in log*.
-
-    This function masks this unharmful warning.
-    """
-    a = np.asarray(a)
     with np.errstate(divide="ignore"):
         return np.log(a)
